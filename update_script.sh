0e9ddd93b216f1dcc65ed4ac896e8d3e44a79188
JakobCh <christoffersson.jakob@gmail.com>
2016-11-14 13:42:52 +0100

    l

--- update_script.sh
+++ update_script.sh
@@ -4,10 +4,16 @@ cp -r rfid_presence rfid_presence_old
 rm -rf rfid_presence
 echo "Getting new.."
 git clone https://github.com/JakobCh/rfid_presence.git
-echo "Copying over databases.."
-cp -r rfid_presence_old/databases/ rfid_presence/
-cp -r rfid_presence_old/exel/ rfid_presence/
-cp rfid_presence_old/MFRC522.py rfid_presence/
-echo "Removing old.."
-rm -rf rfid_presence_old
+if [! -d "rfid_presence"]; then
+	echo "Copying over databases.."
+	cp -r rfid_presence_old/databases/ rfid_presence/
+	cp -r rfid_presence_old/exel/ rfid_presence/
+	cp rfid_presence_old/MFRC522.py rfid_presence/
+	echo "Removing old.."
+	rm -rf rfid_presence_old
+else
+	echo "cound not fetch from github"
+
+fi
+
 echo "Done!"
