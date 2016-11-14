386e11d3fb6bc59804fbd14ad8f349c78c84d384
JakobCh <christoffersson.jakob@gmail.com>
2016-11-14 13:12:25 +0100

    update script

---
+++ update_script.sh
@@ -0,0 +1,10 @@
+echo "Moving old.."
+cp -r rfid_presence rfid_presence_old
+rm -rf rfid_presence
+echo "Getting new.."
+git clone https://github.com/JakobCh/rfid_presence.git
+echo "Copying over databases.."
+cp -r rfid_presence_old/databases/ rfid_presence/
+cp -r rfid_presence_old/exel/ rfid_presence/
+cp rfid_presence_old/MFRC522.py rfid_presence/
+echo "Done!"
