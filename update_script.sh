9ae2e28bd188a4d2ad3d35374f47bbbf50cfb73d
JakobCh <christoffersson.jakob@gmail.com>
2016-11-14 13:19:29 +0100

    updated upgrade script

--- update_script.sh
+++ update_script.sh
@@ -8,5 +8,5 @@ cp -r rfid_presence_old/databases/ rfid_presence/
 cp -r rfid_presence_old/exel/ rfid_presence/
 cp rfid_presence_old/MFRC522.py rfid_presence/
 echo "Removing old.."
-rm -r rfid_presence_old
+rm -rf rfid_presence_old
 echo "Done!"
