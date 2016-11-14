b8080ee75495ac6ef1f97a1e5661f0b478367928
JakobCh <christoffersson.jakob@gmail.com>
2016-11-14 14:43:03 +0100

    added utf-8 declaration

--- autothread.py
+++ autothread.py
@@ -1,3 +1,6 @@
+Ôªø#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import time
 import os
 
@@ -8,16 +11,16 @@ serverip = "10.1.1.15"
 def autothread(checkbase, tagdb):
 	lastwritetime = time.time()
 	while 1:
-		checkbase.updateMaxTime() #kolla om nÂgon har varit inne l‰ngre ‰n en lektion
+		checkbase.updateMaxTime() #kolla om n√•gon har varit inne l√§ngre √§n en lektion
 		
-		if lastwritetime + 60*10 < time.time(): #om det har gÂtt 10 minuter sen sista gÂngen
+		if lastwritetime + 60*10 < time.time(): #om det har g√•tt 10 minuter sen sista g√•ngen
 			os.system("rf -rf " + checkbase.savepath) #ta bort alla exel filer
 			checkbase.cleanup() #ta bort gammla in/ut checkningar
 			checkbase.createexelfiles(tagdb) #skapa nya exel filer
 			
-			os.system('ncftpput -R -u "' + user + '" -p "' + passwd + '" ' + serverip + ' /N‰rvaro/ ' + checkbase.savepath)
+			os.system('ncftpput -R -u "' + user + '" -p "' + passwd + '" ' + serverip + ' /N√§rvaro/ ' + checkbase.savepath)
 			
-			lastwritetime = time.time() #s‰tt ny tid
+			lastwritetime = time.time() #s√§tt ny tid
 			
 			
 
