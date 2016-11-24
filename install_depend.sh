bcdd39f3ae805fe787cb4ab9158b83887b8c0934
JakobCh <christoffersson.jakob@gmail.com>
2016-11-24 13:48:44 +0100

    l

--- install_depend.sh
+++ install_depend.sh
@@ -23,3 +23,5 @@ echo 'else' >> ~/.bashrc
 echo "nohup python -u Main.py run &" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
 echo "cd ~" >> ~/.bashrc
+
+#echo narvaro | sudo sshfs -o allow_other narvaro@10.1.1.15:/mnt/Storage/Larare/ /mnt/Larare/
